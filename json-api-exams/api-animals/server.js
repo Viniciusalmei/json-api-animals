ca4485d24f7121322e95caff7a7305e6ee1cb2d0
Viniciusalmei <vihhalmeida12@gmail.com>
2026-02-12 12:14:37 -0300

    rmv node_modules

---
+++ json-api-exams/api-animals/server.js
@@ -0,0 +1,20 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(jsonServer.bodyParser)
+
+server.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`)
+  next()
+})
+
+server.use(router)
+
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT, () => {
+  console.log(`porta ${PORT}`)
+})
